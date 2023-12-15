05740ae15c2f3771c79e1a0425911099a857ff58
Khushal Patil <58685911+khushalpatil093@users.noreply.github.com>
2023-12-15 19:25:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+
+function incrementValue() {
+	let currentValue = document.createElementById("counter").innerText;
+	let newValue = parseInt(currentValue) + 1;
+
+	document.getElementById("counter").innerText = newValue;
+	alert(currentValue);
+}
